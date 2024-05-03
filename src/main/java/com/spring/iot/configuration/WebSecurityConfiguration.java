59b84e58fbe22f8c43ac89f28006e55a0134e589
NguyenMinhNhat82 <20510520925nhat@ou.edu.vn>
2024-05-03 17:14:28 +0700

    aa

--- src/main/java/com/spring/iot/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/spring/iot/configuration/WebSecurityConfiguration.java
@@ -27,7 +27,7 @@ public class WebSecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/Callback","/authenticate", "/sign-up","/hello","/checkusername/",
+                .requestMatchers("https://serveriot-0z1m.onrender.com/Callback","/authenticate", "/sign-up","/hello","/checkusername/",
                         "/ws/**","/expirationOfToken/**","/refreshtoken/**","/generateOtp"
                         ,"/token-sign-up","/generateOtp","/validateOtp","/admin-authenticate","/api/user/test"
                 ,"/sensor-value/export/excel", "/test", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
