2d0eee6d67a3f124a2bf78a70b246980f567a342
NguyenMinhNhat82 <20510520925nhat@ou.edu.vn>
2024-05-05 07:07:09 +0700

    aa

--- src/main/java/com/spring/iot/dto/VerifyReceiver.java
+++ src/main/java/com/spring/iot/dto/VerifyReceiver.java
@@ -5,14 +5,13 @@
 package com.spring.iot.dto;
 
 
-
 import com.google.api.client.extensions.java6.auth.oauth2.VerificationCodeReceiver;
 import com.google.api.client.util.Throwables;
-import com.sun.net.httpserver.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
@@ -24,8 +23,7 @@ import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Semaphore;
 
-@AllArgsConstructor
-@Data
+
 public final class VerifyReceiver implements VerificationCodeReceiver {
     private static final String LOCALHOST = "localhost";
     private static final String CALLBACK_PATH = "/Callback";
@@ -33,33 +31,78 @@ public final class VerifyReceiver implements VerificationCodeReceiver {
     String code;
     String error;
     final Semaphore waitUnlessSignaled;
+    private int port;
     private final String host;
     private final String callbackPath;
     private String successLandingPageUrl;
     private String failureLandingPageUrl;
 
     public VerifyReceiver() {
-        this("localhost", "/Callback", (String)null, (String)null);
+        this("localhost", -1, "/Callback", (String)null, (String)null);
     }
 
-    VerifyReceiver(String host, String successLandingPageUrl, String failureLandingPageUrl) {
-        this(host, "/Callback", successLandingPageUrl, failureLandingPageUrl);
+    VerifyReceiver(String host, int port, String successLandingPageUrl, String failureLandingPageUrl) {
+        this(host, port, "/Callback", successLandingPageUrl, failureLandingPageUrl);
     }
 
-    VerifyReceiver(String host, String callbackPath, String successLandingPageUrl, String failureLandingPageUrl) {
+    VerifyReceiver(String host, int port, String callbackPath, String successLandingPageUrl, String failureLandingPageUrl) {
         this.waitUnlessSignaled = new Semaphore(0);
         this.host = host;
+        this.port = port;
         this.callbackPath = callbackPath;
         this.successLandingPageUrl = successLandingPageUrl;
         this.failureLandingPageUrl = failureLandingPageUrl;
     }
 
     public String getRedirectUri() throws IOException {
-        return "https://" + this.getHost() + this.callbackPath;
+        this.server = HttpServer.create(new InetSocketAddress(this.port != -1 ? this.port : this.findOpenPort()), 0);
+        HttpContext context = this.server.createContext(this.callbackPath, new CallbackHandler());
+        this.server.setExecutor((Executor)null);
+
+        try {
+            this.server.start();
+            this.port = this.server.getAddress().getPort();
+            System.out.println(server.getAddress().getAddress().getHostName());
+        } catch (Exception var3) {
+            Throwables.propagateIfPossible(var3);
+            throw new IOException(var3);
+        }
+
+        return "http://" + this.getHost() + ":" + this.port + this.callbackPath;
     }
 
+    private int findOpenPort() {
+        try {
+            ServerSocket socket = new ServerSocket(0);
+            Throwable var2 = null;
 
+            int var3;
+            try {
+                socket.setReuseAddress(true);
+                var3 = socket.getLocalPort();
+            } catch (Throwable var13) {
+                var2 = var13;
+                throw var13;
+            } finally {
+                if (socket != null) {
+                    if (var2 != null) {
+                        try {
+                            socket.close();
+                        } catch (Throwable var12) {
+                            var2.addSuppressed(var12);
+                        }
+                    } else {
+                        socket.close();
+                    }
+                }
 
+            }
+
+            return var3;
+        } catch (IOException var15) {
+            throw new IllegalStateException("No free TCP/IP port to start embedded HTTP Server on");
+        }
+    }
 
     public String waitForCode() throws IOException {
         this.waitUnlessSignaled.acquireUninterruptibly();
@@ -89,6 +132,9 @@ public final class VerifyReceiver implements VerificationCodeReceiver {
         return this.host;
     }
 
+    public int getPort() {
+        return this.port;
+    }
 
     public String getCallbackPath() {
         return this.callbackPath;
@@ -99,26 +145,25 @@ public final class VerifyReceiver implements VerificationCodeReceiver {
         }
 
         public void handle(HttpExchange httpExchange) throws IOException {
-            HttpsExchange httpsExchange = (HttpsExchange) httpExchange;
-            if (VerifyReceiver.this.callbackPath.equals(httpsExchange.getRequestURI().getPath())) {
+            if (VerifyReceiver.this.callbackPath.equals(httpExchange.getRequestURI().getPath())) {
                 new StringBuilder();
 
                 try {
-                    Map<String, String> parms = this.queryToMap(httpsExchange.getRequestURI().getQuery());
+                    Map<String, String> parms = this.queryToMap(httpExchange.getRequestURI().getQuery());
                     VerifyReceiver.this.error = (String)parms.get("error");
                     VerifyReceiver.this.code = (String)parms.get("code");
-                    Headers respHeaders = httpsExchange.getResponseHeaders();
+                    Headers respHeaders = httpExchange.getResponseHeaders();
                     if (VerifyReceiver.this.error == null && VerifyReceiver.this.successLandingPageUrl != null) {
                         respHeaders.add("Location", VerifyReceiver.this.successLandingPageUrl);
-                        httpsExchange.sendResponseHeaders(302, -1L);
+                        httpExchange.sendResponseHeaders(302, -1L);
                     } else if (VerifyReceiver.this.error != null && VerifyReceiver.this.failureLandingPageUrl != null) {
                         respHeaders.add("Location", VerifyReceiver.this.failureLandingPageUrl);
-                        httpsExchange.sendResponseHeaders(302, -1L);
+                        httpExchange.sendResponseHeaders(302, -1L);
                     } else {
-                        this.writeLandingHtml(httpsExchange, respHeaders);
+                        this.writeLandingHtml(httpExchange, respHeaders);
                     }
 
-                    httpsExchange.close();
+                    httpExchange.close();
                 } finally {
                     VerifyReceiver.this.waitUnlessSignaled.release();
                 }
@@ -147,12 +192,11 @@ public final class VerifyReceiver implements VerificationCodeReceiver {
         }
 
         private void writeLandingHtml(HttpExchange exchange, Headers headers) throws IOException {
-            HttpsExchange httpsExchange = (HttpsExchange) exchange;
             OutputStream os = exchange.getResponseBody();
             Throwable var4 = null;
 
             try {
-                httpsExchange.sendResponseHeaders(200, 0L);
+                exchange.sendResponseHeaders(200, 0L);
                 headers.add("ContentType", "text/html");
                 OutputStreamWriter doc = new OutputStreamWriter(os, StandardCharsets.UTF_8);
                 doc.write("<html>");
@@ -194,7 +238,7 @@ public final class VerifyReceiver implements VerificationCodeReceiver {
         }
 
         public VerifyReceiver build() {
-            return new VerifyReceiver(this.host, this.callbackPath, this.successLandingPageUrl, this.failureLandingPageUrl);
+            return new VerifyReceiver(this.host, this.port, this.callbackPath, this.successLandingPageUrl, this.failureLandingPageUrl);
         }
 
         public String getHost() {
--- src/main/java/com/spring/iot/services/SheetService.java
+++ src/main/java/com/spring/iot/services/SheetService.java
@@ -5,7 +5,6 @@ import ch.qos.logback.classic.net.server.ServerSocketReceiver;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.java6.auth.oauth2.VerificationCodeReceiver;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
@@ -61,7 +60,8 @@ public class SheetService {
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
                 .setAccessType("offline")
                 .build();
-        VerifyReceiver receiver = new VerifyReceiver.Builder().setHost("serveriot-0z1m.onrender.com").setCallbackPath("/test").build();
+//        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        VerifyReceiver receiver = new VerifyReceiver.Builder().setPort(8888).build();
 
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
