3c9d8345e7f3a0ccd745ea01b3868da9f25da0f7
unknown <2051052092nhat@ou.edu.vn>
2024-05-07 14:45:37 +0700

    aa

--- src/main/java/com/spring/iot/util/JwtUtil.java
+++ src/main/java/com/spring/iot/util/JwtUtil.java
@@ -87,7 +87,7 @@ public class JwtUtil {
                 .setClaims(claims)
                 .setSubject(userName)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*60))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*2))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256).compact();
     }
 
