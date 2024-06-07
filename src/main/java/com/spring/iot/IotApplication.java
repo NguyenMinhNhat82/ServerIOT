0fdc0bcc52c5ad1e7dbc2e8948102652741777a5
NguyenMinhNhat82 <20510520925nhat@ou.edu.vn>
2024-06-07 19:42:31 +0700

    aa

--- src/main/java/com/spring/iot/IotApplication.java
+++ src/main/java/com/spring/iot/IotApplication.java
@@ -19,6 +19,7 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.text.DateFormat;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -62,23 +63,25 @@ public class IotApplication {
     public synchronized void autoGenerateNumber() throws GeneralSecurityException, IOException {
         ZoneId zid = ZoneId.of("Asia/Saigon");
         Random random = new Random();
-        SensorValue sensorValue1 = new SensorValue(0,String.valueOf(random.nextFloat(11) + 20),
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        SensorValue sensorValue1 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(3) + 20)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("temp_0001"));
-        SensorValue sensorValue2 = new SensorValue(0,String.valueOf(random.nextFloat(21) + 60),
+        SensorValue sensorValue2 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(5) + 60)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("humi_0001"));
-        SensorValue sensorValue3 = new SensorValue(0,String.valueOf(random.nextFloat(11) + 20),
+        SensorValue sensorValue3 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(4) + 20)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("temp_0002"));
-        SensorValue sensorValue4 = new SensorValue(0,String.valueOf(random.nextFloat(21) + 60),
+        SensorValue sensorValue4 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(6) + 60)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("humi_0002"));
-        SensorValue sensorValue5 = new SensorValue(0,String.valueOf(random.nextFloat(17) + 4),
+        SensorValue sensorValue5 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(7) + 4)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("ph_0002"));
-        SensorValue sensorValue6 = new SensorValue(0,String.valueOf(random.nextFloat(11)),
+        SensorValue sensorValue6 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(3)+2)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("EC_0002"));
-        SensorValue sensorValue7 = new SensorValue(0,String.valueOf(random.nextFloat(21)),
+        SensorValue sensorValue7 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(3)+4)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("Nito_0002"));
-        SensorValue sensorValue8 = new SensorValue(0,String.valueOf(random.nextFloat(30)),
+        SensorValue sensorValue8 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(30)+20)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("Photpho_0002"));
-        SensorValue sensorValue9 = new SensorValue(0,String.valueOf(random.nextFloat(15)),
+        SensorValue sensorValue9 = new SensorValue(0,String.valueOf(df.format(random.nextFloat(15) + 5)),
                 LocalDateTime.now(zid),sensorService.findSensorByID("Kali_0002"));
         SensorValue sensorValue10 = new SensorValue(0,"true",
                 LocalDateTime.now(zid),sensorService.findSensorByID("Relay_0001"));
@@ -101,7 +104,15 @@ public class IotApplication {
         sensorValueRepository.save(sensorValue11);
         sensorValueRepository.save(sensorValue12);
         sensorValueRepository.save(sensorValue13);
-        String jsonString = "{\"station_id\":\"air_0002\",\"station_name\":\"NBIOT 0002\",\"sensors\":[{\"id\":\"temp_0001\",\"value\":31.3},{\"id\":\"humi_0001\",\"value\":77.3},{\"id\":\"temp_0002\",\"value\":31.9},{\"id\":\"humi_0002\",\"value\":6},{\"id\":\"ph_0002\",\"value\":8.75},{\"id\":\"EC_0002\",\"value\":0},{\"id\":\"Nito_0002\",\"value\":0},{\"id\":\"Photpho_0002\",\"value\":0},{\"id\":\"Kali_0002\",\"value\":0},{\"id\":\"Relay_0001\",\"value\":true},{\"id\":\"Relay_0002\",\"value\":false},{\"id\":\"Relay_0003\",\"value\":false},{\"id\":\"Relay_0004\",\"value\":false}]}";
+        String jsonString = "{\"station_id\":\"air_0002\",\"station_name\":\"NBIOT 0002\",\"sensors\":[{\"id\":\"temp_0001\",\"value\":" + String.valueOf(df.format(random.nextFloat(3) + 20)) +"}" +
+                ",{\"id\":\"humi_0001\",\"value\":"+ String.valueOf(df.format(random.nextFloat(5) + 60))+"}," +
+                "{\"id\":\"temp_0002\",\"value\":" + String.valueOf(df.format(random.nextFloat(4) + 20))+"}," +
+                "{\"id\":\"humi_0002\",\"value\":"+String.valueOf(df.format(random.nextFloat(6) + 60))+"}," +
+                "{\"id\":\"ph_0002\",\"value\":"+String.valueOf(df.format(random.nextFloat(7) + 4))+"}," +
+                "{\"id\":\"EC_0002\",\"value\":"+String.valueOf(df.format(random.nextFloat(3)+2))+"}," +
+                "{\"id\":\"Nito_0002\",\"value\":"+String.valueOf(df.format(random.nextFloat(3)+4))+"}," +
+                "{\"id\":\"Photpho_0002\",\"value\":"+String.valueOf(df.format(random.nextFloat(30)+20))+"}," +
+                "{\"id\":\"Kali_0002\",\"value\":"+String.valueOf(df.format(random.nextFloat(15) + 5))+"},{\"id\":\"Relay_0001\",\"value\":true},{\"id\":\"Relay_0002\",\"value\":false},{\"id\":\"Relay_0003\",\"value\":false},{\"id\":\"Relay_0004\",\"value\":false}]}";
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         Calendar cal = Calendar.getInstance();
         com.spring.iot.dto.Message m = new com.spring.iot.dto.Message("server", "client", jsonString, dateFormat.format(cal.getTime()), Status.MESSAGE);
