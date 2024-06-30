d815fbe727b5ab898464048426db2207ff5acc5b
NguyenMinhNhat82 <20510520925nhat@ou.edu.vn>
2024-06-30 11:51:50 +0700

    aa

--- src/main/java/com/spring/iot/IotApplication.java
+++ src/main/java/com/spring/iot/IotApplication.java
@@ -1,6 +1,7 @@
 package com.spring.iot;
 
 import com.spring.iot.dto.Status;
+import com.spring.iot.entities.Sensor;
 import com.spring.iot.entities.SensorValue;
 import com.spring.iot.repositories.SensorRepository;
 import com.spring.iot.repositories.SensorValueRepository;
@@ -23,9 +24,9 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Random;
+import java.util.*;
+
+import static java.util.Map.entry;
 
 
 @SpringBootApplication
@@ -46,8 +47,102 @@ public class IotApplication {
         SpringApplication.run(IotApplication.class, args);
 
     }
+    static DecimalFormat df = new DecimalFormat("#.##");
+    static Random random = new Random();
+    public static Map<String,String> dataRandom1 = Map.ofEntries(
+            entry("temp_0001", df.format(random.nextFloat(4) + 20)),
+            entry("humi_0001", df.format(random.nextFloat(12) + 5)),// 8-15
+            entry("temp_0002", df.format(random.nextFloat(4) + 20)),
+            entry("humi_0002", df.format(random.nextFloat(12) + 5)),
+            entry("ph_0002", df.format(random.nextFloat(5) + 4)),// 5.5 - 7.5
+            entry("EC_0002", df.format(random.nextFloat(1) + 0.1)),
+            entry("Nito_0002", df.format(random.nextFloat(4) + 60)),
+            entry("Photpho_0002", df.format(random.nextFloat(30) + 20)),
+            entry("Kali_0002", df.format(random.nextFloat(15) + 5)),
+            entry("Relay_0001",  "true" ),
+            entry("Relay_0002",  "true"),
+            entry("Relay_0003",  "true"),
+            entry("Relay_0004",  "true")
+    );
 
-    @Scheduled(fixedRate = 1000*60*4)
+
+    public static Map<String,String> dataRandom2 = Map.ofEntries(
+            entry("temp_0001", df.format(random.nextFloat(4) + 23)),
+            entry("humi_0001", df.format(random.nextFloat(12) + 5)),// 8-15
+            entry("temp_0002", df.format(random.nextFloat(4) + 23)),
+            entry("humi_0002", df.format(random.nextFloat(12) + 5)),
+            entry("ph_0002", df.format(random.nextFloat(5) + 4)),// 5.5 - 7.5
+            entry("EC_0002", df.format(random.nextFloat(1) + 0.1)),
+            entry("Nito_0002", df.format(random.nextFloat(4) + 60)),
+            entry("Photpho_0002", df.format(random.nextFloat(30) + 20)),
+            entry("Kali_0002", df.format(random.nextFloat(15) + 5)),
+            entry("Relay_0001",  "true" ),
+            entry("Relay_0002",  "true"),
+            entry("Relay_0003",  "true"),
+            entry("Relay_0004",  "true")
+    );
+    public static Map<String,String> dataRandom3 = Map.ofEntries(
+            entry("temp_0001", df.format(random.nextFloat(6) + 26)),
+            entry("humi_0001", df.format(random.nextFloat(12) + 5)),// 8-15
+            entry("temp_0002", df.format(random.nextFloat(6) + 26)),
+            entry("humi_0002", df.format(random.nextFloat(12) + 5)),
+            entry("ph_0002", df.format(random.nextFloat(5) + 4)),// 5.5 - 7.5
+            entry("EC_0002", df.format(random.nextFloat(1) + 0.1)),
+            entry("Nito_0002", df.format(random.nextFloat(4) + 60)),
+            entry("Photpho_0002", df.format(random.nextFloat(30) + 20)),
+            entry("Kali_0002", df.format(random.nextFloat(15) + 5)),
+            entry("Relay_0001",  "true" ),
+            entry("Relay_0002",  "true"),
+            entry("Relay_0003",  "true"),
+            entry("Relay_0004",  "true")
+    );
+    public static Map<String,String> dataRandom4 = Map.ofEntries(
+            entry("temp_0001", df.format(random.nextFloat(8) + 29)),
+            entry("humi_0001", df.format(random.nextFloat(12) + 5)),// 8-15
+            entry("temp_0002", df.format(random.nextFloat(8) + 29)),
+            entry("humi_0002", df.format(random.nextFloat(12) + 5)),
+            entry("ph_0002", df.format(random.nextFloat(5) + 4)),// 5.5 - 7.5
+            entry("EC_0002", df.format(random.nextFloat(1) + 0.1)),
+            entry("Nito_0002", df.format(random.nextFloat(4) + 60)),
+            entry("Photpho_0002", df.format(random.nextFloat(29) + 20)),
+            entry("Kali_0002", df.format(random.nextFloat(14) + 5)),
+            entry("Relay_0001",  "true" ),
+            entry("Relay_0002",  "true"),
+            entry("Relay_0003",  "true"),
+            entry("Relay_0004",  "true")
+    );
+    public static Map<String,String> dataRandom5 = Map.ofEntries(
+            entry("temp_0001", df.format(random.nextFloat(4) + 25)),
+            entry("humi_0001", df.format(random.nextFloat(13) + 5)),// 8-15
+            entry("temp_0002", df.format(random.nextFloat(4) + 25)),
+            entry("humi_0002", df.format(random.nextFloat(13) + 5)),
+            entry("ph_0002", df.format(random.nextFloat(6) + 4)),// 5.5 - 7.5
+            entry("EC_0002", df.format(random.nextFloat(2) + 0.1)),
+            entry("Nito_0002", df.format(random.nextFloat(5) + 60)),
+            entry("Photpho_0002", df.format(random.nextFloat(30) + 20)),
+            entry("Kali_0002", df.format(random.nextFloat(15) + 5)),
+            entry("Relay_0001",  "true" ),
+            entry("Relay_0002",  "true"),
+            entry("Relay_0003",  "true"),
+            entry("Relay_0004",  "true")
+    );
+    public static Map<String,String> dataRandom6 = Map.ofEntries(
+            entry("temp_0001", df.format(random.nextFloat(4) + 21)),
+            entry("humi_0001", df.format(random.nextFloat(12) + 5)),// 8-15
+            entry("temp_0002", df.format(random.nextFloat(4) + 21)),
+            entry("humi_0002", df.format(random.nextFloat(12) + 5)),
+            entry("ph_0002", df.format(random.nextFloat(5) + 4)),// 5.5 - 7.5
+            entry("EC_0002", df.format(random.nextFloat(1) + 0.1)),
+            entry("Nito_0002", df.format(random.nextFloat(4) + 60)),
+            entry("Photpho_0002", df.format(random.nextFloat(30) + 20)),
+            entry("Kali_0002", df.format(random.nextFloat(15) + 5)),
+            entry("Relay_0001",  "true" ),
+            entry("Relay_0002",  "true"),
+            entry("Relay_0003",  "true"),
+            entry("Relay_0004",  "true")
+    );
+
+    @Scheduled(fixedRate = 1000*60*2)
     public synchronized void keepServerAlive() throws GeneralSecurityException, IOException {
         RestTemplate restTemplate = new RestTemplate();
         String url =  "https://serveriot-tdz2.onrender.com/test";
@@ -57,6 +152,35 @@ public class IotApplication {
         //https://serveriot-1.onrender.com/test
         ResponseEntity<String> response
                 = restTemplate.getForEntity(url , String.class);
+//        List<Sensor> list  = sensorService.getAll();
+//        for(Sensor s : list) {
+//            if(!s.getId().contains("Relay")) {
+//                for (int i = 9; i <= 23; i++) {
+//                    for (int j = 0; j <= 23; j++) {
+//                        for (int e = 0; e <= 59; e+=3) {
+//                            if(j>=0 && j <=3){
+//
+//                            }
+//                            if(j>=4 && j <=7 ){
+//
+//                            }
+//                            if(j>=8 && j <=11 ){
+//
+//                            }
+//                            if(j>=12 && j <=15 ){
+//
+//                            }
+//                            if(j>=16 && j <=19){
+//
+//                            }
+//                            if(j>= && j <=19){
+//
+//                            }
+//                        }
+//                    }
+//                }
+//            }
+//        }
     }
 
 //    @Scheduled(fixedRate = 1000*60*2)
